2a7aa7d7b29c705b76d2cc05ba3712253559082a
Tristanux <tristanux@gmail.com>
2018-07-12 10:24:49 +0200

    Some tests

---
+++ MasterMind/Start.fs
@@ -0,0 +1,42 @@
+﻿module Start
+
+open Suave
+open Suave.Operators
+open Suave.Filters
+open Suave.Successful
+open System.Linq
+open Newtonsoft.Json.Linq
+
+let sample : WebPart = 
+    path "/hello" >=> choose [
+      GET  >=> OK "GET hello"
+      POST >=> OK "POST hello"
+      RequestErrors.NOT_FOUND "Found no handlers" ]
+
+let other : WebPart = 
+    path "/" >=> choose [
+        GET >=> OK "GET Other"
+        POST >=> OK "POST Other"
+    ]
+
+let jsonTest = @"{
+    'players': ['playerA', 'playerB' ],
+    'turn': 5,
+    'grid' : [ 3, 4, 8, 7, 3 ]
+}"
+
+type game = {
+    players : array<string>
+    turn: int
+    grid: array<int>
+}
+
+let testGame = JObject.Parse(jsonTest).["players"].Select(fun a -> a.ToString())
+printfn "%s" (testGame.ToString())
+
+//printfn "%A" testGame2
+
+//let json : WebPart = 
+//    path "/json" >=> choose [
+//        GET >=> 
+//    ]
--- MasterMind/server.fs
+++ MasterMind/server.fs
@@ -1,14 +1,33 @@
-﻿module server
+﻿module Server
 
 open System
 open System.Threading
 open Suave
+open Suave.Operators
+open System.IO
+open Suave.Filters
+open Start
+open System.IO
+
+Directory.SetCurrentDirectory("../../../")
 
 [<EntryPoint>]
 let main argv = 
   let cts = new CancellationTokenSource()
   let conf = { defaultConfig with cancellationToken = cts.Token }
-  let listening, server = startWebServerAsync conf (Successful.OK "Hello World")
+
+  let app : WebPart =
+      choose [
+        GET >=> path "/" >=> Files.file "public/index.html"
+        sample
+        other
+        //RequestErrors.NOT_FOUND Files.file "public/not_found.html"
+        RequestErrors.NOT_FOUND "Page not found"
+      ]
+  
+  // Combine all the routing parts into a single webpart
+  //let app = staticServe <|> sample <|> other
+  let listening, server = startWebServerAsync conf app
     
   Async.Start(server, cts.Token)
   printfn "Make requests now"
