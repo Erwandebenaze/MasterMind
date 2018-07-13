e04889afc15f266593058f2cb91b7ed70be05af4
Erwan de Bénazé <duhildebenaze@intechinfo.fr>
2018-07-13 12:28:36 +0200

    Push pour la note mais rien ne marche :)

--- Elm/login.elm
+++ Elm/login.elm
@@ -1,16 +1,17 @@
-import Html exposing (Html, div, input, text)
+import Html exposing (Html, button, div, input, text)
 import Html.Attributes exposing (..)
-import Html.Events exposing (onInput)
+import Html.Events exposing (onInput, onClick)
 import String
-
+import Http
 -- Get sur /login 
 -- ?name='nomDuJoueur'
 
 main =
-  Html.beginnerProgram
+  Html.program
     { model = model
     , view = view
     , update = update
+    , subscriptions = subscriptions
     }
 
 
@@ -19,7 +20,7 @@ main =
 
 
 type alias Model =
-  { content : String
+  { playerName : String
   }
 
 
@@ -34,12 +35,20 @@ model =
 
 type Msg
   = Change String
+  | Send (Result Http.Error String)
 
 update : Msg -> Model -> Model
 update msg model =
   case msg of
     Change newContent ->
-      { model | content = newContent }
+      { model | playerName = newContent }
+
+    Send (Ok name) ->
+      ( { model | playerName = name }, Cmd.none)
+
+    Send (Err _) ->
+      (model, Cmd.none)
+
 
 
 
@@ -49,6 +58,27 @@ update msg model =
 view : Model -> Html Msg
 view model =
   div []
-    [ input [ placeholder "Text to reverse", onInput Change ] []
-    , div [] [ text (String.reverse model.content) ]
+    [ input [ placeholder "Nom du joueur", onInput Change ] []
+    , button [ onClick Send ] [text "Envoyer"]
     ]
+
+-- SUBSCRIPTIONS
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+  Sub.none
+
+
+
+-- HTTP
+
+
+getRandomGif : String -> Cmd Msg
+getRandomGif topic =
+  let
+    url =
+      "https://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=" ++ topic
+  in
+    Http.send Send (model | playerName)
+
